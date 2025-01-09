2bf3b93abdd4c11a4ad5855465c8c544e49b3b8c
sgd27 <sgd27@users.noreply.github.com>
2025-01-09 09:06:49 +0000

    Libbox Release 1.10.6

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Libbox",
-      url: "https://github.com/sgd27/sing-box-lib/releases/download/1.10.1/Libbox.xcframework.zip",
-      checksum: "023edeb192e391c54f0e8089999f99b965e7042a7a2d6cdb74f7bf6b3ba973fd"
+      url: "https://github.com/sgd27/sing-box-lib/releases/download/1.10.6/Libbox.xcframework.zip",
+      checksum: ""
     )
   ]
 )
